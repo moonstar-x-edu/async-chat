2f0e1d88625ef5ff49e54808c0e1434d6fe80648
moonstar-x <christian.lopez99@outlook.com>
2022-04-25 15:30:40 -0500

    Added server message handling.

--- server/chat/connection.py
+++ server/chat/connection.py
@@ -1,6 +1,12 @@
 from socket import socket
 from threading import Thread
 
+TAG_MSG = 'MSG'
+TAG_ERR = 'ERR'
+TAG_CFG = 'CFG'
+TAG_CMD = 'CMD'
+TAG_LIST = 'LIST'
+
 
 class Connection(Thread):
     def __init__(self, server, client_socket: socket, client_address: tuple):
@@ -11,6 +17,11 @@ class Connection(Thread):
         self.socket = client_socket
         self.address = client_address
         self.username = None
+
+        self.peer = None
+
+    def is_engaged(self):
+        return self.peer is not None
 
     def run(self) -> None:
         try:
@@ -27,16 +38,45 @@ class Connection(Thread):
         tag = received_message[:separator_index]
         received_message = received_message[separator_index + 1:]
 
-        if tag == 'MSG':
+        if tag == TAG_MSG:
             return self._handle_message(received_message)
-        if tag == 'CMD':
+        if tag == TAG_CMD:
             return self._handle_command(received_message)
+        if tag == TAG_CFG:
+            return self._handle_config(received_message)
 
     def _handle_message(self, message: str) -> None:
-        self._send_to_socket('MSG', message)
+        if not self.is_engaged():
+            return self._send_to_socket(TAG_ERR, 'You are not connected to anybody else.')
+
+        # Send message to recipient.
 
     def _handle_command(self, command: str) -> None:
-        self._send_to_socket('ERR', command)
+        if command == 'list':
+            return self._send_to_socket(TAG_LIST, self.server.get_online_list())
+
+        if command.startswith('connect'):
+            # Handle client connection.
+            return self._send_to_socket(TAG_CMD, 'SUCCESS')
+
+        if command == 'disconnect':
+            # Handle client disconnection.
+            return self._send_to_socket(TAG_CMD, 'SUCCESS')
+
+        return self._send_to_socket(TAG_ERR, 'Invalid CMD message sent.')
+
+    def _handle_config(self, config: str) -> None:
+        if config.startswith('set_username'):
+            username = config[:config.index('set_username')]
+
+            if self.server.get_connection_by_username(username) is not None:
+                return self._send_to_socket(TAG_ERR, 'Username already in use.')
+
+            self.username = username
+            self.log('INFO', f'Saved username for client as {self.username}')
+            return self._send_to_socket(TAG_CFG, 'SUCCESS')
+
+        return self._send_to_socket(TAG_ERR, 'Invalid CFG message sent.')
 
     def _receive_from_socket(self) -> str:
         if self.socket is None:
